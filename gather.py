fb59526d6eb0604934f68b24f9c982f4d3c5fb47
Joan Marc Riera Duocastella <joanmarcriera@gmail.com>
2018-03-26 23:28:51 +0100

    function

--- gather.py
+++ gather.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 from selenium import webdriver
+#import pdb; pdb.set_trace()
 keyWords=['Catalonia','Puigdemont']
-options = webdriver.ChromeOptions()
-options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
-options.add_argument('headless')
-options.add_argument('window-size=600x400')
-driver=webdriver.Chrome(chrome_options=options)
-driver.get('http://hvper.com')
+
+
+def run_browser(headless=True,site='http://hvper.com'):
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+    if headless: options.add_argument('headless')
+    options.add_argument('window-size=600x400')
+    driver=webdriver.Chrome(chrome_options=options)
+    driver.get(site)
+    return driver
+
 def act_on_link(link):
     print ("["+link.text+"]("+link.get_attribute("href")+")")
     print ()
+
+driver = run_browser()
+
 for word in keyWords:
     links = driver.find_elements_by_xpath("//a[@href]")
     [ act_on_link(link) for link in links if any(word in link.text for word in keyWords)]
